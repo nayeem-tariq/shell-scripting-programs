e343146ae423345b8043ff8bbb463296a1712632
nayeem_tariq <nayeem.tariq@outlook.com>
2024-08-13 19:22:51 +0530

    upload

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+clear
 read -p "Enter num1 " x
 read -p "Enter num2 " y
 read -p "Select your option (1.Addition,2.subtraction,3.Multiplication,4.Division: )" opt
@@ -6,19 +7,19 @@ read -p "Select your option (1.Addition,2.subtraction,3.Multiplication,4.Divisio
 case $opt in
 	1)
 		echo "You selected Addition"
-		echo "Addition of $x and $y is $(bc <<<"$x+$y")"
+		echo "Addition of $x and $y is $(bc <<<"scale=1;$x+$y")"
 		;;
         2)
                 echo "You selected subtraction"
-                echo "Subtraction of $x and $y is $(bc <<<"$x-$y")"
+                echo "Subtraction of $x and $y is $(bc <<<"scale=1;$x-$y")"
                 ;;
         3)
                 echo "You selected Multiplication"
-                echo "Multiplication of $x and $y is $(bc <<<"$x*$y")"
+                echo "Multiplication of $x and $y is $(bc <<<"scale=1;$x*$y")"
                 ;;
         4) 
                 echo "You selected Division"
-                echo "Division of $x and $y is $(bc <<<"$x/$y")"
+                echo "Division of $x and $y is $(bc <<<"scale=1;$x/$y")"
                 ;;
         *)
                 echo "You selected a invalid option"
